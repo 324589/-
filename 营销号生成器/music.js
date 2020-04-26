2cf8fd761e57305533aad8fb477521462e53b3b2
MineCreeper86 <61276297+MineCreeper86@users.noreply.github.com>
2020-04-26 10:48:09 +0800

    Add files via upload

---
+++ 营销号生成器/music.js
@@ -0,0 +1,19 @@
+    let i = Math.floor(Math.random()*6);
+    if(parseInt(i)==0){
+      document.getElementById("musical").src = "trip.mp3";
+    }
+	if(parseInt(i)==1){
+      document.getElementById("musical").src = "a little story.mp3";
+    }
+	if(parseInt(i)==2){
+      document.getElementById("musical").src = "bgm3.mp3";
+    }
+	if(parseInt(i)==3){
+      document.getElementById("musical").src = "bgm4.mp3";
+    }
+	if(parseInt(i)==4){
+      document.getElementById("musical").src = "bgm5.mp3";
+    }
+	if(parseInt(i)==5){
+      document.getElementById("musical").src = "bgm6.mp3";
+    }
