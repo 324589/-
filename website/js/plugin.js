900646276139b7a3a3f078bb4ffe9357afdae9a1
324589 <63893884+324589@users.noreply.github.com>
2020-07-04 10:55:02 +0800

    Add files via upload

---
+++ website/js/plugin.js
@@ -0,0 +1,21 @@
++(function($){
+	var Pic=function(){
+		$(".pic").mouseenter(function(){
+		});	
+		$(".pic").mouseleave()(function(){
+		});	
+	}
+
+	$(window).on('load',function(){
+		Pic();
+		$(".btt").hide();
+	});
+	$(window).scroll(function(){
+		if($(window).scrollTop()>500){
+			$(".btt").show();	
+		}
+		else{
+			$(".btt").hide();
+		}
+	});
+})(jQuery);
